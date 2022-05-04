fdbe24fa70d6033635439b025763a291e84e34ce
Raymond Ndibe <ndiberaymond1@gmail.com>
2022-05-03 18:12:01 -0700

    update test

--- src/renderer/renderless/FilesProcessor.test.js
+++ src/renderer/renderless/FilesProcessor.test.js
@@ -7,7 +7,7 @@ import { default_state as query } from '../store/reducers/queryReducers';
 import { default_state as workspace } from '../store/reducers/workSpaceReducers';
 import { testStore } from '../assets/js/utils/testUtils';
 // import { isFilePathInQueryResult, openFile, refreshRecent, refreshOpenFiles } from '../assets/js/utils/scripts';
-import * as scripts from '../assets/js/utils/scripts';
+import {refreshOpenFiles, refreshRecent} from '../assets/js/utils/scripts';
 import { processFiles } from './filesProcessorScripts';
 
 // import {
@@ -21,10 +21,16 @@ import { processFiles } from './filesProcessorScripts';
 //   isFileInRecentlyOpened: jest.fn(() => {}),
 // }));
 
-jest.mock('./filesProcessorScripts', ()=> ({
+jest.mock('./filesProcessorScripts', () => ({
 	__esmodule: true,
-	processFiles: jest.fn( async ()=> {})
-}))
+	processFiles: jest.fn(async () => {}),
+}));
+
+jest.mock('../assets/js/utils/scripts', () => ({
+	__esModule: true,
+	refreshRecent: jest.fn(() => {}),
+	refreshOpenFiles: jest.fn(() => {}),
+}));
 
 const setUp = (initialState = {}) => {
 	const store = testStore(initialState);
@@ -34,84 +40,25 @@ const setUp = (initialState = {}) => {
 
 describe('FileProcessor component:', () => {
 	let wrapper;
-	let isFilePathInQueryResult;
-	let openFile;
-	let refreshRecent;
-	let refreshOpenFiles;
 
 	beforeEach(() => {
-		// jest.mock('../assets/js/utils/scripts', () => ({
-		//   __esModule: true,
-		//   isFilePathInQueryResult: jest.fn(() => false),
-		//   openFile: jest.fn(),
-		//   refreshRecent: jest.fn(),
-		//   refreshOpenFiles: jest.fn()
-		// }));
-
-		openFile = jest.spyOn(scripts, 'openFile').mockImplementation(() => {});
-		refreshRecent = jest
-			.spyOn(scripts, 'refreshRecent')
-			.mockImplementation(() => {});
-		refreshOpenFiles = jest
-			.spyOn(scripts, 'refreshOpenFiles')
-			.mockImplementation(() => {});
-	});
-
-	it('expect refreshRecent to have been called', () => {
-		isFilePathInQueryResult = jest
-			.spyOn(scripts, 'isFilePathInQueryResult')
-			.mockImplementation(() => false);
 		const wrapper_and_store = setUp({ files, query, workspace });
 		wrapper = wrapper_and_store.wrapper;
+	});
 
+	it('expect refreshRecent to have been called', () => {	
 		expect(refreshRecent).toHaveBeenCalled();
 	});
 
 	it('expect refreshOpenFiles to have been called', () => {
-		isFilePathInQueryResult = jest
-			.spyOn(scripts, 'isFilePathInQueryResult')
-			.mockImplementation(() => false);
-		const wrapper_and_store = setUp({ files, query, workspace });
-		wrapper = wrapper_and_store.wrapper;
-
 		expect(refreshOpenFiles).toHaveBeenCalled();
 	});
 
-	it('expect isFilePathInQueryResult to have been called', () => {
-		isFilePathInQueryResult = jest
-			.spyOn(scripts, 'isFilePathInQueryResult')
-			.mockImplementation(() => false);
-		const wrapper_and_store = setUp({ files, query, workspace });
-		wrapper = wrapper_and_store.wrapper;
-
-		expect(isFilePathInQueryResult).toHaveBeenCalled();
-	});
-
-	it('expect openFile to not have been called', () => {
-		isFilePathInQueryResult = jest
-			.spyOn(scripts, 'isFilePathInQueryResult')
-			.mockImplementation(() => false);
-		const wrapper_and_store = setUp({ files, query, workspace });
-		wrapper = wrapper_and_store.wrapper;
-
-		expect(openFile).not.toHaveBeenCalled();
-	});
-
-	it('expect openFile to have been called with "/a/b/c/d"', () => {
-		isFilePathInQueryResult = jest
-			.spyOn(scripts, 'isFilePathInQueryResult')
-			.mockImplementation(() => '/a/b/c/d');
-		const wrapper_and_store = setUp({ files, query, workspace });
-		wrapper = wrapper_and_store.wrapper;
-
-		expect(openFile).toHaveBeenCalledWith('/a/b/c/d');
-	});
+	it('expect processFiles to have been called', ()=> {
+		expect(processFiles).toHaveBeenCalled();
+	})
 
 	afterEach(() => {
 		wrapper.unmount();
-		isFilePathInQueryResult.mockRestore();
-		openFile.mockRestore();
-		refreshRecent.mockRestore();
-		refreshOpenFiles.mockRestore();
 	});
 });
